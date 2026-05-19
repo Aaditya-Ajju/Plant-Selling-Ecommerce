eb95a7fc307d9f7ee256288a6f4f901075af6237
Aaditya-Ajju <aadityarajwebdev@gmail.com>
2026-05-19 16:37:48 +0530

    fixed kv issue

--- backend/src/utils/redisVercelKv.js
+++ backend/src/utils/redisVercelKv.js
@@ -1,30 +1,18 @@
 const { kv } = require('@vercel/kv'); // Import the appropriate Redis client library
 
 async function getData(userId, orderToken, key) {
-    try {
-        const result = await kv.json.get(`${process.env.REDIS_VERCEL_KV_DB}:${userId}:${orderToken}:${key}`);
-
-        if (!result) {
-            const error = new Error(`Data from redis not found: key - ${key}`);
-            error.statusCode = 403;
-            throw error;
-        }
-
-        return result;
-
-    } catch (error) {
-        // Pass error to error handling middleware
-        console.log(error);
-    }
+    const result = await kv.json.get(`${process.env.REDIS_VERCEL_KV_DB}:${userId}:${orderToken}:${key}`);
+    return result || null;
 }
 
 async function setData(userId, token, key, data, expire) {
     try {
-        await kv.json.set(`${process.env.REDIS_VERCEL_KV_DB}:${userId}:${token}:${key}`, "$", JSON.stringify(data));
+        await kv.json.set(`${process.env.REDIS_VERCEL_KV_DB}:${userId}:${token}:${key}`, "$", data);
         await kv.expire(`${process.env.REDIS_VERCEL_KV_DB}:${userId}:${token}:${key}`, expire);
     } catch (error) {
         // Pass error to error handling middleware
         console.log(error);
+        throw error;
     }
 
 }
